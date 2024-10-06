f5c73a10cbf526be32ddaedf573d418866d8ea9d
Yogev Shalmon <yogev.shalmon@intel.com>
2024-10-06 11:21:34 +0300

    Start bool match alg base

---
+++ src/BoolMatchAlg/BoolMatchAlgBase.cpp
@@ -0,0 +1,151 @@
+#include "BoolMatchAlg/BoolMatchAlgBase.hpp"
+
+#include "Globals/BoolMatchAlgGlobals.hpp"
+#include "Utilities/StringUtilities.hpp"
+
+using namespace std;
+using namespace lorina;
+
+
+BoolMatchAlgBase::BoolMatchAlgBase(const InputParser& inputParser):
+// default is not printing
+m_PrintMatches(inputParser.getBoolCmdOption("/general/print_matches", true)),
+// if timeout was given
+m_UseTimeOut(inputParser.cmdOptionExists("/general/timeout")),
+// check if timeout is given in command
+m_TimeOut(inputParser.getUintCmdOption("/general/timeout", DEF_TIMEOUT)),
+m_IsTimeOut(false), 
+m_TimeOnGeneralization(0),
+m_NumberOfMatches(0)
+{
+    m_Clk = clock();
+}
+
+BoolMatchAlgBase::~BoolMatchAlgBase() 
+{
+
+}
+
+void BoolMatchAlgBase::PrintInitialInformation()
+{
+    cout << "c Start enumerating all matches" << endl;
+    #ifdef DEBUG
+        cout << "c Tool is compiled in Debug" << endl;
+    #endif
+
+    #ifdef SAT_SOLVER_INDEX_64
+        cout << "c Tool is compiled in 64 bit index mode" << endl;
+    #endif
+
+    #ifdef SAT_SOLVER_COMPRESS
+        cout << "c Tool is compiled in compress mode" << endl;
+    #endif
+    if (m_UseTimeOut)
+    {
+        cout << "c Timeout: " << m_TimeOut << " seconds" << endl;
+    }  
+}
+
+
+void BoolMatchAlgBase::ParseAigFile(const string& filename, AigerParser& aigParser)
+{
+    return_code result;
+    
+    if (stringEndsWith(filename, ".aag"))
+    {
+        result = read_ascii_aiger(filename, aigParser);
+    }
+    else if (stringEndsWith(filename, ".aig"))
+    {
+        result = read_aiger(filename, aigParser);
+    }
+    else
+    {
+        throw runtime_error("Unkonw aiger format, please provide either .aag or .aig file");
+    }
+
+    if ( result == return_code::parse_error )
+    {
+        throw runtime_error("Error parsing the file");
+    }
+
+}
+
+void BoolMatchAlgBase::PrintResult(bool wasInterrupted)
+{
+    bool isInterrupted = m_IsTimeOut || wasInterrupted;
+    unsigned long cpu_time =  clock() - m_Clk;
+    double Time = (double)(cpu_time)/(double)(CLOCKS_PER_SEC);
+    if (isInterrupted)
+    {
+        cout << "c *** Interrupted *** " << endl;
+    }
+    cout << "c Number of matches: " << m_NumberOfMatches;
+    if (isInterrupted)
+    {
+        cout << "+";
+    }
+    cout << endl;
+    cout << "c Percentage of time spent on generalization: " << m_TimeOnGeneralization/Time;
+
+    cout << endl;
+    cout << "c cpu time : " << Time <<" sec" << endl;
+}
+
+// print value of a single AIG index
+// base function for all implementation
+void BoolMatchAlgBase::PrintIndexVal(const AIGINDEX litIndex, const TVal& currVal)
+{
+    if (currVal == TVal::True)
+    {
+        cout << litIndex << " ";
+    }
+    else if (currVal == TVal::False)
+    {
+        cout << "-" << litIndex << " ";
+    }
+    else if (currVal == TVal::DontCare) // dont care case
+    {
+        // Note: for now print nothing
+        //cout << "x ";
+    }
+    else
+    {
+        throw runtime_error("Unkown value for input");
+    }
+}
+
+void BoolMatchAlgBase::PrintLitVal(const AIGLIT lit, const TVal& currVal)
+{
+    if (IsAIGLitNeg(lit))
+    {
+        PrintIndexVal(AIGLitToAIGIndex(lit), GetTValNeg(currVal));
+    }
+    else
+    {
+        PrintIndexVal(AIGLitToAIGIndex(lit), currVal);
+    }
+}
+
+void BoolMatchAlgBase::PrintModel(const INPUT_ASSIGNMENT& model)
+{   
+    // model can be partial assignment, we only print Bool values, aka no DC
+    for (const pair<AIGLIT, TVal>& assign : model)
+    {
+        PrintLitVal(assign.first, assign.second);
+    }
+    
+    cout << endl;
+}
+
+
+unsigned BoolMatchAlgBase::GetNumOfDCFromInputAssignment(const INPUT_ASSIGNMENT& assignment) const
+{
+    // count number of 1/0 values
+    int numOfBoolVal = count_if(assignment.begin(), assignment.end(), [](const pair<AIGLIT, TVal>& assign)
+    { 
+         return ((assign.second == TVal::True) || (assign.second == TVal::False)); 
+    });
+    assert(numOfBoolVal >= 0);
+    return (unsigned)m_InputSize - (unsigned)numOfBoolVal;
+}
---
+++ src/BoolMatchAlg/BoolMatchAlgBase.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include "Globals/BoolMatchGloblas.hpp"
+#include "Globals/BoolMatchSolverGloblas.hpp"
+#include "Aiger/AigerParser.hpp"
+#include "Utilities/InputParser.hpp"
+
+/*
+    base class for boolmatch algorithm
+    provide some general functonallity
+*/
+class BoolMatchAlgBase 
+{
+    public:
+
+        AllSatAlgoBase(const InputParser& inputParser);
+
+        virtual ~BoolMatchAlgBase();
+
+        // intilize with aiger files for both the src and trg
+        virtual void InitializeFromAIGs(const std::string& srcFileName, const std::string& trgFileName) = 0;
+
+        // find all the boolean matches for the given AIGs 
+        virtual void FindAllMatches() = 0;
+
+        void PrintResult(bool wasInterrupted = false);
+
+    protected:
+
+        // print initial information, timeout etc..
+        virtual void PrintInitialInformation();
+        
+        // parse aag or aig files
+        // initilize either the m_AigParserSrc and m_AigParserTrg
+        void ParseAigFile(const std::string& filename, AigerParser& aigParser);
+
+        // print single model for the inputs
+        void PrintModel(const INPUT_ASSIGNMENT& model);
+        
+        // print value of a single AIG index
+        void PrintIndexVal(const AIGINDEX litIndex, const TVal& currVal);
+
+        // print value of a single AIG lit
+        void PrintLitVal(const AIGLIT lit, const TVal& currVal);
+
+        unsigned GetNumOfDCFromInputAssignment(const INPUT_ASSIGNMENT& assignment) const;
+
+        // *** Params ***
+
+        // if to print the enumerated matches
+        const bool m_PrintMatches;
+        // if timeout was given
+        const bool m_UseTimeOut;
+        // timeout
+        const double m_TimeOut;
+		
+        // *** Variables ***
+        
+        // parser for Aiger files 
+        AigerParser m_AigParserSrc;
+        AigerParser m_AigParserTrg;
+
+        // original inputs
+        std::vector<AIGLIT> m_Inputs;
+        // size of m_Inputs
+        size_t m_InputSize;
+
+		// *** Stats ***
+
+		clock_t m_Clk;
+        // if timeout happend
+        bool m_IsTimeOut;
+        // time spent on generalization
+        double m_TimeOnGeneralization;
+        // number of matches found
+        unsigned long long m_NumberOfMatches;
+};
