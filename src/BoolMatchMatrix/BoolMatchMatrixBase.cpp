52466d930950e92293f645ac50e49ed90568cc62
Yogev Shalmon <yogev.shalmon@intel.com>
2024-10-09 07:50:20 +0300

    Implement basic func for the base matrix class

--- src/BoolMatchMatrix/BoolMatchMatrixBase.cpp
+++ src/BoolMatchMatrix/BoolMatchMatrixBase.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 BoolMatchMatrixBase::BoolMatchMatrixBase(BoolMatchSolverBase* solver, unsigned inputSize, const MatrixIndexVecMatch& indexMapping, bool useMatchSelector, bool allowNegMap):
-m_Solver(solver),
-m_InputSize(inputSize),
 m_UseMatchSelector(useMatchSelector),
-m_NegMapIsAllowed(allowNegMap)
+m_NegMapIsAllowed(allowNegMap),
+m_Solver(solver),
+m_InputSize(inputSize)
 {
     m_DataMatchMatrix = new MatrixIndexVars[m_InputSize];
     for (unsigned i = 0; i < m_InputSize; ++i)
@@ -19,4 +19,61 @@ m_NegMapIsAllowed(allowNegMap)
         m_MatchSelector = m_Solver->GetNewVar();
     }
 
+}
+
+BoolMatchMatrixBase::~BoolMatchMatrixBase()
+{
+    delete[] m_DataMatchMatrix;
+}
+
+SOLVER_RET_STATUS BoolMatchMatrixBase::FindNextMatch()
+{
+	SOLVER_RET_STATUS res = ERR_RET_STATUS;
+	res = m_UseMatchSelector ? m_Solver->SolveUnderAssump({m_MatchSelector}) : m_Solver->Solve();
+	return res;
+}
+
+
+void BoolMatchMatrixBase::EliminateMatches(const vector<MatrixIndexVecMatch>& matchesToElim, const bool ignoreSelector)
+{
+	for (const MatrixIndexVecMatch& matchToElim : matchesToElim)
+	{
+		EliminateMatch(matchToElim, ignoreSelector);
+	}
+}
+
+void BoolMatchMatrixBase::AssertNoMatch()
+{
+	if (m_UseMatchSelector)
+	{
+		m_Solver->AddClause({CONST_LIT_FALSE, NegateSATLit(m_MatchSelector)});
+	}
+	else
+	{
+		m_Solver->AddClause(CONST_LIT_FALSE);
+	}
+}
+
+void BoolMatchMatrixBase::ResetEliminatedMatches()
+{
+    assert(m_UseMatchSelector);
+    // remove the selector
+    m_Solver->AddClause(NegateSATLit(m_MatchSelector));
+    // create new selector
+    m_MatchSelector = m_Solver->GetNewVar();
+}
+
+size_t BoolMatchMatrixBase::GerMatrixSize() const
+{
+	return (size_t)GetMatrixColRowSize() * (size_t)GetMatrixColRowSize();
+}
+
+size_t BoolMatchMatrixBase::GetAbsMatrixPosFromIndexes(const MatrixIndexMatch& match) const
+{
+	return GetAbsMatrixPosFromIndexes(match.first, match.second);
+}
+
+size_t BoolMatchMatrixBase::GetAbsMatrixPosFromIndexes(int x, int y) const
+{
+	return (GetAbsRealIndex(x) * GetMatrixColRowSize()) + GetAbsRealIndex(y);
 }
--- src/BoolMatchMatrix/BoolMatchMatrixBase.hpp
+++ src/BoolMatchMatrix/BoolMatchMatrixBase.hpp
@@ -11,10 +11,6 @@ enum class BoolMatchBlockType
     ENFORCE_MATCH,
     DYNAMIC_BLOCK
 };
-
-// the minimum group size for dynamic block to choose blocking instead of enforcing
-static const size_t DYNAMIC_BLOCK_MIN_GROUP_SIZE = 4;
-
 class BoolMatchMatrixBase
 {
 public:
@@ -33,12 +29,81 @@ public:
     // allowNegMap - if to allow to find new neg map (not included given indexMapping)
     BoolMatchMatrixBase(BoolMatchSolverBase* m_Solver, unsigned inputSize, const MatrixIndexVecMatch& indexMapping, bool useMatchSelector, bool allowNegMap);
 
-    virtual ~BoolMatchMatrixBase()
-    {
-        delete[] m_DataMatchMatrix;
-    }
+    virtual ~BoolMatchMatrixBase();
+
+    // try to find a next valid match
+    // return the status of the next SAT call (which will try to find a new match)
+    // SAT: meaning there is a match
+    // UnSAT: no more matches
+    // Timeout: Timeout
+    // NOTE: we need to block the found match otherwise can get the same one
+    SOLVER_RET_STATUS FindNextMatch();
+
+    // get the current match
+    // will be implemented in the derived classes 
+    virtual MatrixIndexVecMatch GetCurrMatch() const = 0;
+
+    // eliminate a single match, i.e. [(1,3),(2,1),..]
+    // the function can be used to eliminate the current match
+    // this will ensure that the match will not be found again in the next call (FindNextMatch)
+    // NOTE: only one match can be "ignored" i.e. eliminating [(1,3),(2,1),..] can cause the next search to find [(1,3),(2,2),..]
+    // ignoreSelector - if to ignore the selector (if exist) when eliminating the match
+    //					this is useful when we want to eliminate the match without the selector
+    // will be implemented in the derived classes	
+    virtual void EliminateMatch(const MatrixIndexVecMatch& matchToElim, const bool ignoreSelector = false) = 0;
+
+    // the input vector contain vectors of matches that cant be together.
+    // for example { {{1,2}}, {{3,2},{4,5}} } -> match(1,2) and match(3,2)&match(4,5) will be eliminted
+    // call EliminateMatch iteratively
+    void EliminateMatches(const std::vector<MatrixIndexVecMatch>& matchesToElim, const bool ignoreSelector = false);
+
+    // enforce a single match, i.e. [(1,3),(2,1)] mean (1,3) -or- (2,1) must exist
+    virtual void EnforceMatch(const MatrixIndexVecMatch& matchToEnforce) = 0;
+
+    // assert that no match can be found by just asserting false
+    void AssertNoMatch();
+
+    // return m_MatchSelector
+    SATLIT GetMatchSelector() {return m_MatchSelector;};
+
+    // reset the matches eliminted with the selector, also create a new one
+    // Note: the function will work only if selector was created, otherwise it will throw an exception
+    void ResetEliminatedMatches();
+
+    bool IsNegMatchAllowed() {return m_NegMapIsAllowed;};
+
+    // return the number of created blocked clauses to block matches
+    unsigned long long GetNumOfBlockedClsMatches() const {return m_NumOfBlockedClsMatches;};
 
 protected:
+
+    // *** Functions ***
+
+    size_t GerMatrixSize() const;
+    // index start from 1 since we canot have 0 and -0
+    inline static unsigned GetFirstIndex() {return (unsigned)1;};
+    // get the size of the matrix, will be the same for the row and col
+    inline unsigned GetMatrixColRowSize() const {return m_InputSize;};
+
+    // since the matrix is actually a single dimentional array we need to get the position in the array
+    size_t GetAbsMatrixPosFromIndexes(const MatrixIndexMatch& match) const;
+    size_t GetAbsMatrixPosFromIndexes(MatrixIndex x, MatrixIndex y) const;
+
+    // create new vars and assert exactly 1 on every col and row
+    // indexMapping: if given index mapping is not empty assert the mapping
+    virtual void AssertRowAndCol(const MatrixIndexVecMatch& indexMapping) = 0;
+
+
+    // *** Params ***
+
+    // if to use match selector
+    const bool m_UseMatchSelector;
+    // if neg map is allowed
+    const bool m_NegMapIsAllowed;
+
+    // the minimum group size for dynamic block to choose blocking instead of enforcing
+    static const size_t DYNAMIC_BLOCK_MIN_GROUP_SIZE = 4;
+
     // *** Variables ***
 
     BoolMatchSolverBase* m_Solver;
@@ -47,16 +112,12 @@ protected:
     // the actual matrix, each index will hold the match variables
     MatrixIndexVars* m_DataMatchMatrix;
 
-    // if to use match selector
-    const bool m_UseMatchSelector;
     // this will be the selector for the valid matches
     // when calling EliminateMatches the clauses will be generated with this selector
     // call SAT with this as assumptions, when wanting to remove just Assert(~m_MatchSelector)
     // NOTE: this will be create each time we reset
     SATLIT m_MatchSelector;
 
-    // if neg map is allowed
-    const bool m_NegMapIsAllowed;
 
     // *** Stats ***
 
