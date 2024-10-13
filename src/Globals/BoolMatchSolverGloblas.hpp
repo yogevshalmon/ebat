cb862b893c322f2c3d79bff30197dd225c1465d0
Yogev Shalmon <yogev.shalmon@intel.com>
2024-10-13 15:18:52 +0300

    Add INDX_ASSIGNMENT

--- src/Globals/BoolMatchSolverGloblas.hpp
+++ src/Globals/BoolMatchSolverGloblas.hpp
@@ -20,7 +20,12 @@ typedef int SOLVER_RET_STATUS;
 // assignment for input use the AIGLIT of the input as index and TVal as value
 // can be partial assignment for the inputs where DC is allowed, if input not exist consider as DC
 // all variables reprsent some original input
-typedef std::vector<std::pair<AIGLIT, TVal>> INPUT_ASSIGNMENT;
+using INPUT_ASSIGNMENT = std::vector<std::pair<AIGLIT, TVal>>;
+
+// assignment where each original AIGLIT is converted to the its index according to the input list
+// can be partial assignment for the inputs where DC is allowed, if input not exist consider as DC
+// all variables reprsent some original input
+using INDX_ASSIGNMENT = std::vector<std::pair<unsigned, TVal>>;
 
 static constexpr SOLVER_RET_STATUS SAT_RET_STATUS = 10;
 static constexpr SOLVER_RET_STATUS UNSAT_RET_STATUS = 20;
