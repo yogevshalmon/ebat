69b1523e78c1afda8fde1818442f4fd1169843fd
Yogev Shalmon <yogev.shalmon@intel.com>
2024-10-30 09:09:25 +0200

    Create generic class to hold all the generalization func instead of iter and block

---
+++ src/BoolMatchAlg/GeneralizationEnumer/BoolMatchAlgGenEnumerBase.cpp
@@ -0,0 +1,156 @@
+#include "BoolMatchAlg/GeneralizationEnumer/BoolMatchAlgGenEnumerBase.hpp"
+
+#include "BoolMatchMatrix/BoolMatchMatrixSingleVars/BoolMatchMatrixSingleVars.hpp"
+
+using namespace std;
+
+// TODO add to input parser /alg/gen_enumer/.... 
+BoolMatchAlgGenEnumerBase::BoolMatchAlgGenEnumerBase(const InputParser& inputParser):
+BoolMatchAlgBase(inputParser),
+// defualt is false
+m_UseCirSim(inputParser.getBoolCmdOption("/alg/use_cirsim", false)),
+// default is true
+m_UseTopToBotSim(inputParser.getBoolCmdOption("/alg/use_top_to_bot_sim", true)),
+// default is false
+m_UseDualSolver(inputParser.getBoolCmdOption("/alg/use_ucore", false)),
+// default is true
+m_UseLitDrop(inputParser.getBoolCmdOption("/alg/use_lit_drop", true)),
+// default is 0 i.e. none
+m_LitDropConflictLimit(inputParser.getUintCmdOption("/alg/lit_drop_conflict_limit", 0)),
+// default is false
+m_LitDropChekRecurCore(inputParser.getBoolCmdOption("/alg/lit_drop_recur_ucore", false)),
+m_Solver(nullptr), 
+m_DualSolver(nullptr),
+m_InputMatchMatrix(nullptr),
+m_SrcCirSimulation(nullptr),
+m_TrgCirSimulation(nullptr)
+{
+    // we can not use cir simulation or core generalization if negated map is not allowed
+    // this is because we can have a situation where the we have 00XX -> 11XX (was 0011 -> 1100) and we can not block it under the assumption that no negated map is allowed
+    if (!m_AllowInputNegMap && (m_UseCirSim || m_UseDualSolver))
+    {
+        throw runtime_error("Can not use circuit simulation or UnSAT core generalization if negated map is not allowed");
+    }
+}
+
+BoolMatchAlgGenEnumerBase::~BoolMatchAlgGenEnumerBase() 
+{
+    delete m_Solver;
+    delete m_DualSolver;
+
+    delete m_InputMatchMatrix;
+
+    delete m_SrcCirSimulation;
+    delete m_TrgCirSimulation;
+}
+
+void BoolMatchAlgGenEnumerBase::PrintResult(bool wasInterrupted)
+{
+    BoolMatchAlgBase::PrintResult(wasInterrupted);
+    
+    m_InputMatchMatrix->PrintStats();
+}
+
+
+void BoolMatchAlgGenEnumerBase::_InitializeFromAIGs()
+{
+    // initilize cir simulation if needed
+    if (m_UseCirSim)
+    {
+        m_SrcCirSimulation = new CirSim(m_AigParserSrc, m_UseTopToBotSim ? SimStrat::TopToBot : SimStrat::BotToTop);
+        m_TrgCirSimulation = new CirSim(m_AigParserTrg, m_UseTopToBotSim ? SimStrat::TopToBot : SimStrat::BotToTop);
+    }
+
+    m_Solver->InitializeSolverFromAIG(m_AigParserSrc, m_AigParserTrg);
+
+    if (m_UseDualSolver)
+    {
+        m_DualSolver->InitializeSolverFromAIG(m_AigParserSrc, m_AigParserTrg);
+    }
+
+    _InitMatchMatrix();
+}
+
+
+void BoolMatchAlgGenEnumerBase::_FindAllMatches()
+{
+    SOLVER_RET_STATUS res = m_Solver->Solve();
+
+    if (res == TIMEOUT_RET_STATUS || m_IsTimeOut)
+    {
+        m_IsTimeOut = true;
+        throw runtime_error("Timeout reached");
+    }
+
+    if (res != SAT_RET_STATUS)
+	{
+		throw runtime_error("Initial model is not satisfiable. Please verify the logic model of the cells are correct.");
+	}
+
+    // TODO handle here the output assumption for plain and dual solvers
+    // assert mitter on the circuit outputs
+    m_Solver->AssertOutputDiff(false);
+    if (m_UseDualSolver) m_DualSolver->AssertOutputDiff(true);
+
+    FindAllMatchesUnderOutputAssert();
+};
+
+
+void BoolMatchAlgGenEnumerBase::PrintInitialInformation()
+{
+    BoolMatchAlgBase::PrintInitialInformation();
+
+    if (m_UseCirSim)
+    {
+        if (m_UseTopToBotSim)
+        {
+            cout << "c Use Top to Bottom simulation" << endl;
+        }
+        else
+        {
+            cout << "c Use Bottom to Top simulation" << endl;
+        }
+    }
+    if (m_UseDualSolver)
+    {
+        cout << "c Use dual solver for unSAT-core" << endl;
+        if (m_UseLitDrop)
+        {
+            cout << "c Use literal dropping for unSAT-core" << endl;
+            if (m_LitDropConflictLimit > 0)
+            {
+                cout << "c Limit conflict in literal dropping to " << m_LitDropConflictLimit << endl;
+            }
+            if (m_LitDropChekRecurCore)
+            {
+                cout << "c Use recursive unSAT-core check in literal dropping" << endl;
+            } 
+        }
+    }
+}
+
+
+INPUT_ASSIGNMENT BoolMatchAlgGenEnumerBase::GeneralizeWithCirSimulation(const INPUT_ASSIGNMENT& model, CirSim* cirSim)
+{
+    // use simulation for maximize dont-care values
+    // we do not assume the output should remain 1
+    return cirSim->MaximizeDontCare(model, false);
+}
+
+pair<INPUT_ASSIGNMENT, INPUT_ASSIGNMENT> BoolMatchAlgGenEnumerBase::GeneralizeModel(const INPUT_ASSIGNMENT& srcAssg, const INPUT_ASSIGNMENT& trgAssg)
+{ 
+    INPUT_ASSIGNMENT generalizeSrcModel = srcAssg;
+    INPUT_ASSIGNMENT generalizeTrgModel = trgAssg;
+    if (m_UseCirSim)
+    {
+        generalizeSrcModel = GeneralizeWithCirSimulation(generalizeSrcModel, m_SrcCirSimulation);
+        generalizeTrgModel = GeneralizeWithCirSimulation(generalizeTrgModel, m_TrgCirSimulation);
+    }
+    if (m_UseDualSolver)
+    {
+        pair<INPUT_ASSIGNMENT, INPUT_ASSIGNMENT> generalizedModels = m_DualSolver->GetUnSATCore(generalizeSrcModel, generalizeTrgModel, m_UseLitDrop, m_LitDropConflictLimit, m_LitDropChekRecurCore);
+        generalizeSrcModel = generalizedModels.first;
+        generalizeTrgModel = generalizedModels.second;
+    }
+    return make_pair(generalizeSrcModel, generalizeTrgModel);
+};
---
+++ src/BoolMatchAlg/GeneralizationEnumer/BoolMatchAlgGenEnumerBase.hpp
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "BoolMatchAlg/BoolMatchAlgBase.hpp"
+#include "BoolMatchSolver/Solvers.hpp"
+#include "BoolMatchMatrix/BoolMatchMatrixBase.hpp"
+#include "CirSimulation/CirSim.hpp"
+
+/*
+    Base solver class for any algorithm that will use enumeratioon the possible matrix matches
+    and will use generalization for the CEX (counter-example/non-valid) models
+*/
+class BoolMatchAlgGenEnumerBase : public BoolMatchAlgBase
+{
+    public:
+
+        BoolMatchAlgGenEnumerBase(const InputParser& inputParser);
+
+        virtual ~BoolMatchAlgGenEnumerBase();
+
+        void PrintResult(bool wasInterrupted = false);
+
+    protected:
+
+        // print initial information, timeout etc..
+        virtual void PrintInitialInformation();
+
+        virtual void _InitializeFromAIGs();
+
+        // initlize the match matrix
+        virtual void _InitMatchMatrix() = 0;
+
+        // find all the boolean matches for the given AIGs
+        virtual void _FindAllMatches();
+
+        // find all the boolean matches for the given AIGs
+        // after we assert that the output diff
+        virtual void FindAllMatchesUnderOutputAssert() = 0;
+        
+        INPUT_ASSIGNMENT GeneralizeWithCirSimulation(const INPUT_ASSIGNMENT& model, CirSim* cirSim);
+
+        // generalize model of src and trg
+        // return the generalized assignment for the src and trg in the form of <src, trg>
+        std::pair<INPUT_ASSIGNMENT, INPUT_ASSIGNMENT> GeneralizeModel(const INPUT_ASSIGNMENT& srcAssg, const INPUT_ASSIGNMENT& trgAssg);
+        
+        // *** Params ***
+
+        // if to use simulation
+        const bool m_UseCirSim;
+        // if to use top to bottom simulation
+        const bool m_UseTopToBotSim;
+        // if to use dual solver for unsat core
+        const bool m_UseDualSolver;
+        // if to use literal dropping in unsat core
+        const bool m_UseLitDrop;
+        // if > 0 use conflict limit when use drop lit in unsat core
+        const unsigned m_LitDropConflictLimit;
+        // if to check unsat core with each drop lit check
+        const bool m_LitDropChekRecurCore;
+  
+		
+        // *** Variables ***
+        
+        // solver for the original circuits mitter
+        BoolMatchSolverBase* m_Solver;
+        // solver for the dual circuits mitter, used for ucore extraction
+        BoolMatchSolverBase* m_DualSolver;
+
+        // the match matrix for the src-trg inputs
+        BoolMatchMatrixBase* m_InputMatchMatrix;
+
+        // cir simulation component for the src and trg circuits
+        CirSim* m_SrcCirSimulation;
+        CirSim* m_TrgCirSimulation;
+
+
+		// *** Stats ***
+
+};
